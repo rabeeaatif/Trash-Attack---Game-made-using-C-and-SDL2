e72183e134d82f40c985f5b8421ebe42f5df4654
Farah Naz Khan <54754403+fk05164@users.noreply.github.com>
2019-11-27 11:38:13 +0500

    Add files via upload

--- LTexture.cpp
+++ LTexture.cpp
@@ -1,154 +1,109 @@
-
-#include <SDL.h>
-#include <SDL_image.h>
-#include <stdio.h>
-#include <string>
-#include "LTexture.hpp"
-
+#include"LTexture.hpp"
 
 LTexture::LTexture()
 {
-	//Initialize
-	mTexture = NULL;
-	mWidth = 0;
-	mHeight = 0;
+  texture = NULL;
+  width = 0;
+  height = 0;
 }
 
 LTexture::~LTexture()
 {
-	//Deallocate
-	free();
+  Free();
 }
 
-bool LTexture::loadFromFile(std::string path)
+bool LTexture::LoadFromFile( std::string path , SDL_Renderer* gRenderer)
 {
-	//Get rid of preexisting texture
-	free();
+  //Get rid of preexisting texture
+  Free();
 
-	//The final texture
-	SDL_Texture *newTexture = NULL;
+  //The final texture
+  SDL_Texture* newTexture = NULL;
 
-	//Load image at specified path
-	SDL_Surface *loadedSurface = IMG_Load(path.c_str());
-	if (loadedSurface == NULL)
-	{
-		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
-	}
-	else
-	{
-		//Color key image
-		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+  //Load image at specified path
+  SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+  if( loadedSurface == NULL )
+    {
+      printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+    }
+  else
+    {
+      //Color key image
+      SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
+      //Create texture from surface pixels
+      newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
+      if( newTexture == NULL )
+        {
+	  printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+        }
+      else
+        {
+	  //Get image dimensions
+	  width = loadedSurface->w;
+	  height = loadedSurface->h;
+        }
 
-		//Create texture from surface pixels
-		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
-		if (newTexture == NULL)
-		{
-			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
-		}
-		else
-		{
-			//Get image dimensions
-			mWidth = loadedSurface->w;
-			mHeight = loadedSurface->h;
-		}
+      //Get rid of old loaded surface
+      SDL_FreeSurface( loadedSurface );
+    }
 
-		//Get rid of old loaded surface
-		SDL_FreeSurface(loadedSurface);
-	}
-
-	//Return success
-	mTexture = newTexture;
-	return mTexture != NULL;
+  //Return success
+  texture = newTexture;
+  return texture != NULL;
 }
 
-#if defined(_SDL_TTF_H) || defined(SDL_TTF_H)
-bool LTexture::loadFromRenderedText(std::string textureText, SDL_Color textColor)
+void LTexture::Free()
 {
-	//Get rid of preexisting texture
-	free();
-
-	//Render text surface
-	SDL_Surface *textSurface = TTF_RenderText_Solid(gFont, textureText.c_str(), textColor);
-	if (textSurface != NULL)
-	{
-		//Create texture from surface pixels
-		mTexture = SDL_CreateTextureFromSurface(gRenderer, textSurface);
-		if (mTexture == NULL)
-		{
-			printf("Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			//Get image dimensions
-			mWidth = textSurface->w;
-			mHeight = textSurface->h;
-		}
-
-		//Get rid of old surface
-		SDL_FreeSurface(textSurface);
-	}
-	else
-	{
-		printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
-	}
-
-	//Return success
-	return mTexture != NULL;
-}
-#endif
-
-void LTexture::free()
-{
-	//Free texture if it exists
-	if (mTexture != NULL)
-	{
-		SDL_DestroyTexture(mTexture);
-		mTexture = NULL;
-		mWidth = 0;
-		mHeight = 0;
-	}
+  //Free texture if it exists
+  if( texture != NULL )
+    {
+      SDL_DestroyTexture( texture );
+      texture = NULL;
+      width = 0;
+      height = 0;
+    }
 }
 
-void LTexture::setColor(Uint8 red, Uint8 green, Uint8 blue)
+void LTexture::SetColor( Uint8 red, Uint8 green, Uint8 blue )
 {
-	//Modulate texture rgb
-	SDL_SetTextureColorMod(mTexture, red, green, blue);
+  //Modulate texture rgb
+  SDL_SetTextureColorMod( texture, red, green, blue );
 }
 
-void LTexture::setBlendMode(SDL_BlendMode blending)
+void LTexture::SetBlendMode( SDL_BlendMode blending )
 {
-	//Set blending function
-	SDL_SetTextureBlendMode(mTexture, blending);
+  //Set blending function
+  SDL_SetTextureBlendMode( texture, blending );
 }
 
-void LTexture::setAlpha(Uint8 alpha)
+void LTexture::SetAlpha( Uint8 alpha )
 {
-	//Modulate texture alpha
-	SDL_SetTextureAlphaMod(mTexture, alpha);
+  //Modulate texture alpha
+  SDL_SetTextureAlphaMod( texture, alpha );
 }
 
-void LTexture::render(int x, int y, SDL_Rect *clip, double angle, SDL_Point *center, SDL_RendererFlip flip)
+int LTexture::GetWidth()
 {
-	//Set rendering space and render to screen
-	SDL_Rect renderQuad = {x, y, mWidth, mHeight};
-
-	//Set clip rendering dimensions
-	if (clip != NULL)
-	{
-		renderQuad.w = clip->w;
-		renderQuad.h = clip->h;
-	}
-
-	//Render to screen
-	SDL_RenderCopyEx(gRenderer, mTexture, clip, &renderQuad, angle, center, flip);
+  return width;
 }
 
-int LTexture::getWidth()
+int LTexture::GetHeight()
 {
-	return mWidth;
+  return height;
 }
 
-int LTexture::getHeight()
+void LTexture::Render( int x, int y, SDL_Rect* clip , double angle, SDL_Point* center, SDL_RendererFlip flip, SDL_Renderer* gRenderer)
 {
-	return mHeight;
+  //Set rendering space and render to screen
+  SDL_Rect renderQuad = { x, y, width, height };
+
+  //Set clip rendering dimensions
+  if( clip != NULL )
+    {
+      renderQuad.w = clip->w;
+      renderQuad.h = clip->h;
+    }
+
+  //Render to screen
+  SDL_RenderCopyEx( gRenderer, texture, clip, &renderQuad, angle, center, flip );
 }
--- LTexture.hpp
+++ LTexture.hpp
@@ -1,53 +1,41 @@
 #pragma once
-
+#ifdef __APPLE__
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#else
 #include <SDL.h>
+#include <SDL_image.h>
+#endif
+#include <cstdio>
 #include <iostream>
-#include "Point.hpp"
 
+//Texture wrapper class
 class LTexture
 {
 private:
-	//The actual hardware texture
-	SDL_Texture *mTexture;
-
-	//Image dimensions
-	int mWidth;
-	int mHeight;
-
-
+  SDL_Texture* texture;
+  int width;
+  int height;
 public:
-	//Initializes variables
-	LTexture();
+  LTexture();
+  ~LTexture();
 
-	//Deallocates memory
-	~LTexture();
+  bool LoadFromFile( std::string path, SDL_Renderer* gRenderer );
 
-	//Loads image at specified path
-	bool loadFromFile(std::string path);
+  void Free();    //Deallocates texture
 
-#if defined(_SDL_TTF_H) || defined(SDL_TTF_H)
-	//Creates image from font string
-	bool loadFromRenderedText(std::string textureText, SDL_Color textColor);
-#endif
+  //Set color modulation
+  void SetColor( Uint8 red, Uint8 green, Uint8 blue );
 
-	//Deallocates texture
-	void free();
+  //Set blending
+  void SetBlendMode( SDL_BlendMode blending );
 
-	//Set color modulation
-	void setColor(Uint8 red, Uint8 green, Uint8 blue);
+  //Set alpha modulation
+  void SetAlpha( Uint8 alpha );
 
-	//Set blending
-	void setBlendMode(SDL_BlendMode blending);
+  void Render( int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE, SDL_Renderer* gRenderer = NULL);
 
-	//Set alpha modulation
-	void setAlpha(Uint8 alpha);
-
-	//Renders texture at given point
-	void render(int x, int y, SDL_Rect *clip = NULL, double angle = 0.0, SDL_Point *center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
-
-	//Gets image dimensions
-	int getWidth();
-	int getHeight();
+  int GetWidth();
+  int GetHeight();
 
 };
-
