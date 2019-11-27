208a7123f3014a850382a4794c3989b963985d60
Swaleha-12 <54802743+Swaleha-12@users.noreply.github.com>
2019-11-27 11:55:59 +0500

    updated point.hpp

--- Point.hpp
+++ Point.hpp
@@ -1,17 +1,12 @@
 #pragma once
 #include <iostream>
 
-class Point
+struct Point
 {
-    private:
-    
-        float x;
-        float y;
-
-    public:
-
-        Point();
-        float GetX();
-        float GetY();
-
+    float m_x;
+    float m_y;
+    Point() : m_x(0), m_y(0){}
+    Point (float x, float y) : m_x(x), m_y(y) {}
+    float GetX() {return m_x;}
+    float GetY() {return m_y;}
 };
