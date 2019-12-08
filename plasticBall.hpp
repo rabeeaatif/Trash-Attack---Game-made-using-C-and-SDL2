d40cc1ad47e55e55f2141dbbf24a934f94692db5
rabeeaatif <54802882+rabeeaatif@users.noreply.github.com>
2019-12-08 01:50:38 -0800

    Create plasticBall.hpp

---
+++ plasticBall.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ball.hpp"
+
+class plasticBall : public ball
+{
+public:
+    plasticBall(LTexture *image, float x, float y);
+    plasticBall();
+    virtual ~plasticBall();
+    void Move();
+    virtual void Render(long int &frame, SDL_Renderer *gRenderer, bool debug);
+};
