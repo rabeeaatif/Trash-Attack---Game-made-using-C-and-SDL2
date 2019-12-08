2e9fd2c5e54623c8dc0520ad325b30adc5de748f
rabeeaatif <54802882+rabeeaatif@users.noreply.github.com>
2019-12-08 01:40:55 -0800

    Updated Player with the relevant functions

--- Player.hpp
+++ Player.hpp
@@ -1,35 +1,34 @@
-#include <SDL.h>
-#include <SDL_image.h>
-#include <stdio.h>
 #pragma once
 
-class Player
+#ifdef __APPLE__
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#else
+#include <SDL.h>
+#include <SDL_image.h>
+#endif
+
+#include <cstdio>
+#include <iostream>
+#include "Unit.hpp"
+#include "LTexture.hpp"
+
+using namespace std;
+
+class Player : public Unit
 {
-public:
-	//The dimensions of the dot
-	static const int DOT_WIDTH = 20;
-	static const int DOT_HEIGHT = 20;
-
-	//Maximum axis velocity of the dot
-	static const int DOT_VEL = 10;
-
-	//Initializes the variables
-
-	Player();
-
-	//Takes key presses and adjusts the dot's velocity
-	void handleEvent(SDL_Event &e);
-
-	//Moves the dot
-	void move();
-
-	//Shows the dot on the screen
-	void render();
-
 private:
-	//The X and Y offsets of the dot
-	int mPosX, mPosY;
+    enum ANIMATION_FRAMES
+    {
+        MOVING_FRAMES = 9
+    };
+    SDL_Rect spriteClips[MOVING_FRAMES];
 
-	//The velocity of the dot
-	int mVelX, mVelY;
+public:
+    Player(LTexture *image, float x, float y);
+    Player();
+    virtual ~Player();
+    virtual void Move(int direction);
+    void Move();
+    void Render(long int &frame, SDL_Renderer *gRenderer, bool debug);
 };
