4d25d478c7c72af9c2bcdb42591e982d5c377b02
rabeeaatif <54802882+rabeeaatif@users.noreply.github.com>
2019-12-08 01:43:36 -0800

    Update Metal.hpp

--- Metal.hpp
+++ Metal.hpp
@@ -1,8 +1,14 @@
+#include <iostream>
 #include "Trash.hpp"
 
 class Metal : public Trash
 {
+private:
+public:
     Metal();
-    ~Metal();
-    void move();
+    Metal(LTexture *bodyImg, float x, float y);
+    virtual ~Metal();
+    virtual void Render(long int &frame, SDL_Renderer *gRenderer, bool debug);
+    virtual void Move();
+    void Draw(SDL_Renderer *gRenderer);
 };
