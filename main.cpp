6f2bab403bb844b1be8a203c277309cd16011921
rabeeaatif <54802882+rabeeaatif@users.noreply.github.com>
2019-12-08 01:32:40 -0800

    Updated main with player and moving trash

--- main.cpp
+++ main.cpp
@@ -1,205 +1,284 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2019)
-and may not be redistributed without written permission.*/
-
-//Using SDL, SDL_image, standard IO, vectors, and strings
+#ifdef __APPLE__
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#else
 #include <SDL.h>
 #include <SDL_image.h>
-#include <stdio.h>
-#include <string>
+#endif
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <list>
 #include "LTexture.hpp"
-#include "Player.hpp"
+#include "player.hpp"
+#include "Unit.hpp"
+#include "Metal.hpp"
+#include "Paper.hpp"
+#include "Plastic.hpp"
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+//Pre defined screen width and height
+const int SCREEN_WIDTH = 1024;
+const int SCREEN_HEIGHT = 600;
 
-
-//Starts up SDL and creates window
 bool init();
 
-//Loads media
 bool loadMedia();
 
-//Frees media and shuts down SDL
 void close();
 
-//The window we'll be rendering to
 SDL_Window *gWindow = NULL;
 
-//The window renderer
 SDL_Renderer *gRenderer = NULL;
 
-//Scene textures
-LTexture gDotTexture;
-LTexture gBGTexture;
+LTexture gSpriteSheetTexture;
+LTexture gBGtexture;
+LTexture gPaper;
+LTexture gMetal;
+LTexture gPlastic;
 
+bool init();
+bool loadMedia();
+void close();
+
+int main(int argc, char *args[])
+{
+    //Start up SDL and create window
+    if (!init())
+    {
+        printf("Failed to initialize!\n");
+    }
+    else
+    {
+        //Load media
+        if (!loadMedia())
+        {
+            printf("Failed to load media!\n");
+        }
+        else
+        {
+
+            bool quit = false; //Main loop flag
+            list<Trash *> trashlist;
+            SDL_Event e; //Event handler
+            long int trashframe = 0;
+            long int delaytrash = 0;
+            long int frame = 0; //Current animation frame
+            int ballDelay = 0;
+            int animationRate = 20;
+            int animationLength = 8;
+            int startTime = SDL_GetTicks();
+            int scrollingOffset = 0;
+
+            /* initialize random seed: */
+            srand(time(NULL));
+
+            /* generate secret number between 1 and 10: */
+            int random = 0;
+
+            //Queue objectList;
+
+            Unit *player = new Player(&gSpriteSheetTexture, (float)SCREEN_WIDTH / 2, (float)SCREEN_HEIGHT - 80);
+            //Unit *metal = NULL;
+            Trash *trash = NULL;
+            //   Unit* enemy = NULL;
+            //   Unit* bullet = NULL;
+
+            while (!quit) //While application is running
+            {
+                int trashrand = rand() % 3;
+                if (delaytrash % 200 == 0)
+                {
+                    random = rand() % SCREEN_WIDTH;
+                    if (trashrand == 0)
+                    {
+                        trash = new Paper(&gPaper, random, -100);
+                    }
+                    else if (trashrand == 1)
+                    {
+                        trash = new Metal(&gMetal, random, -100);
+                    }
+                    else if (trashrand == 2)
+                    {
+                        trash = new Plastic(&gPlastic, random, -100);
+                    }
+                    trashlist.push_back(trash);
+                }
+                while (SDL_PollEvent(&e) != 0) //Handle events on queue
+                {
+
+                    cout << "Enter poll event";
+                    //User requests quit
+                    if (e.type == SDL_QUIT)
+                    {
+                        quit = true;
+                    }
+                }
+                ++scrollingOffset;
+                if (scrollingOffset > gBGtexture.GetHeight())
+                {
+                    scrollingOffset = 0;
+                }
+
+                const Uint8 *currentKeyStates = SDL_GetKeyboardState(NULL);
+
+                if (currentKeyStates[SDL_SCANCODE_RIGHT])
+                {
+                    player->Move(RIGHT);
+                }
+
+                if (currentKeyStates[SDL_SCANCODE_LEFT])
+                {
+                    player->Move(LEFT);
+                }
+
+                if (currentKeyStates[SDL_SCANCODE_SPACE])
+                {
+                    //   if(bulletDelay > 60)
+                    //     {
+                    //       bullet = new Bullet(&gSpriteSheetTexture,plane->GetX(), plane->GetY() - plane->GetHeight()/2);
+                    //       objectList.Enqueue(bullet);
+                    //       bulletDelay = 0;
+                    //     }
+                }
+
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF); //Clear screen
+                SDL_RenderClear(gRenderer);
+
+                //   objectList.Render(frame, gRenderer, false);
+                //   objectList.Move();
+                gBGtexture.Render(0, scrollingOffset, NULL, 0.0, NULL, SDL_FLIP_NONE, gRenderer, 0);
+                gBGtexture.Render(0, scrollingOffset - gBGtexture.GetWidth() + 210, NULL, 0.0, NULL, SDL_FLIP_VERTICAL, gRenderer, 0);
+                player->Render(frame, gRenderer, false);
+                //paper->Render(trashframe, gRenderer, false);
+                for (std::list<Trash *>::iterator it = trashlist.begin(); it != trashlist.end(); ++it)
+                {
+
+                    (*it)->Render(trashframe, gRenderer, false);
+                    (*it)->Move();
+                }
+
+                //objectList.Clean();
+                SDL_RenderPresent(gRenderer); //Update screen
+                frame = ((SDL_GetTicks() - startTime) * animationRate / 1000) % animationLength;
+                ++delaytrash;
+                cout << frame << "\n";
+                //++frame;                            //Go to next frame
+                //++bulletDelay;
+            }
+            delete player;
+        }
+    }
+
+    //Free resources and close SDL
+    close();
+
+    return 0;
+}
 
 bool init()
 {
-	//Initialization flag
-	bool success = true;
+    //Initialization flag
+    bool success = true;
 
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
-		success = false;
-	}
-	else
-	{
-		//Set texture filtering to linear
-		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
-		{
-			printf("Warning: Linear texture filtering not enabled!");
-		}
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    else
+    {
+        //Set texture filtering to linear
+        if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+        {
+            printf("Warning: Linear texture filtering not enabled!");
+        }
 
-		//Create window
-		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (gWindow == NULL)
-		{
-			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
-			success = false;
-		}
-		else
-		{
-			//Create vsynced renderer for window
-			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-			if (gRenderer == NULL)
-			{
-				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
-				success = false;
-			}
-			else
-			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        //Create window
+        gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (gWindow == NULL)
+        {
+            printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+            success = false;
+        }
+        else
+        {
+            //Create renderer for window
+            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            if (gRenderer == NULL)
+            {
+                printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+                success = false;
+            }
+            else
+            {
+                //Initialize renderer color
+                SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
-				//Initialize PNG loading
-				int imgFlags = IMG_INIT_PNG;
-				if (!(IMG_Init(imgFlags) & imgFlags))
-				{
-					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
-					success = false;
-				}
-			}
-		}
-	}
+                //Initialize PNG loading
+                int imgFlags = IMG_INIT_PNG;
+                if (!(IMG_Init(imgFlags) & imgFlags))
+                {
+                    printf("SDL_image could not initialize! SDL_mage Error: %s\n", IMG_GetError());
+                    success = false;
+                }
+            }
+        }
+    }
 
-	return success;
+    return success;
 }
 
 bool loadMedia()
 {
-	//Loading success flag
-	bool success = true;
+    //Loading success flag
+    bool success = true;
 
-	//Load dot texture
-	if (!gDotTexture.loadFromFile("dot2.bmp"))
-	{
-		printf("Failed to load dot texture!\n");
-		success = false;
-	}
+    //Load sprite sheet texture
+    if (!gSpriteSheetTexture.LoadFromFile("Sprite1.png", gRenderer))
+    {
+        printf("Failed to load sprite sheet texture!\n");
+        success = false;
+    }
+    if (!gBGtexture.LoadFromFile("dg.png", gRenderer))
+    {
+        printf("Failed to load background texture!\n");
+        success = false;
+    }
+    if (!gPaper.LoadFromFile("images/paper1.png", gRenderer))
+    {
+        printf("Failed to load paper texture!\n");
+        success = false;
+    }
+    if (!gMetal.LoadFromFile("images/tincan.png", gRenderer))
+    {
+        printf("Failed to load paper texture!\n");
+        success = false;
+    }
+    if (!gPlastic.LoadFromFile("images/pb1.png", gRenderer))
+    {
+        printf("Failed to load paper texture!\n");
+        success = false;
+    }
 
-	//Load background texture
-	if (!gBGTexture.loadFromFile("dg.png"))
-	{
-		printf("Failed to load background texture!\n");
-		success = false;
-	}
-
-	return success;
+    return success;
 }
 
 void close()
 {
-	//Free loaded images
-	gDotTexture.free();
-	gBGTexture.free();
+    //Free loaded images
+    gSpriteSheetTexture.Free();
+    gPaper.Free();
 
-	//Destroy window
-	SDL_DestroyRenderer(gRenderer);
-	SDL_DestroyWindow(gWindow);
-	gWindow = NULL;
-	gRenderer = NULL;
+    //Destroy window
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+    gRenderer = NULL;
 
-	//Quit SDL subsystems
-	IMG_Quit();
-	SDL_Quit();
-}
-
-int main(int argc, char *args[])
-{
-	//Start up SDL and create window
-	if (!init())
-	{
-		printf("Failed to initialize!\n");
-	}
-	else
-	{
-		//Load media
-		if (!loadMedia())
-		{
-			printf("Failed to load media!\n");
-		}
-		else
-		{
-			//Main loop flag
-			bool quit = false;
-
-			//Event handler
-			SDL_Event e;
-
-			//The dot that will be moving around on the screen
-			Player dot;
-
-			//The background scrolling offset
-			int scrollingOffset = 0;
-
-			//While application is running
-			while (!quit)
-			{
-				//Handle events on queue
-				while (SDL_PollEvent(&e) != 0)
-				{
-					//User requests quit
-					if (e.type == SDL_QUIT)
-					{
-						quit = true;
-					}
-
-					//Handle input for the dot
-					dot.handleEvent(e);
-				}
-
-				//Move the dot
-				dot.move();
-
-				//Scroll background
-				--scrollingOffset;
-				if (scrollingOffset < -gBGTexture.getWidth())
-				{
-					scrollingOffset = 0;
-				}
-
-				//Clear screen
-				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-				SDL_RenderClear(gRenderer);
-
-				//Render background
-				gBGTexture.render(0, scrollingOffset);
-				gBGTexture.render(0, scrollingOffset + gBGTexture.getWidth());
-
-				//Render objects
-				dot.render();
-
-				//Update screen
-				SDL_RenderPresent(gRenderer);
-			}
-		}
-	}
-
-	//Free resources and close SDL
-	close();
-
-	return 0;
+    //Quit SDL subsystems
+    IMG_Quit();
+    SDL_Quit();
 }
