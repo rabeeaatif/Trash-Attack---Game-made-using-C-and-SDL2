c2d89f7bb4cc7ff0af0a424c724cfd4999f0d1c1
rabeeaatif <54802882+rabeeaatif@users.noreply.github.com>
2019-12-08 01:34:58 -0800

    Updated Trash  with the relevant functions.

--- Trash.hpp
+++ Trash.hpp
@@ -1,9 +1,23 @@
-#include "Object.hpp"
+#pragma once
+#include "Unit.hpp"
+#include "LTexture.hpp"
 
-class Trash : public Object
+class Trash: public Unit
 {
+protected:
+    enum ANIMATION_FRAMES
+    {
+        MOVING_FRAMES = 1
+    };
+    
+    SDL_Rect spriteClips[MOVING_FRAMES];
+
 public:
     Trash();
-    ~Trash();
-    void move();
+    Trash(LTexture* bodyImg,  float x, float y);
+    virtual ~Trash();
+    void Draw(SDL_Renderer* gRenderer);
+    virtual void Move();
+    virtual void Render(long int &frame, SDL_Renderer *gRenderer, bool debug);
+
 };
