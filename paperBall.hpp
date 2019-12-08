a9c3c5561d37eb37f46dfb840e8d2de7866dfcdd
rabeeaatif <54802882+rabeeaatif@users.noreply.github.com>
2019-12-08 01:49:58 -0800

    Create paperBall.hpp

---
+++ paperBall.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ball.hpp"
+
+class paperBall : public ball
+{
+public:
+    paperBall(LTexture *image, float x, float y);
+    paperBall();
+    virtual ~paperBall();
+    void Move();
+    virtual void Render(long int &frame, SDL_Renderer *gRenderer, bool debug);
+};
