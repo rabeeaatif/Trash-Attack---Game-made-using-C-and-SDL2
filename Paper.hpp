23a9de914f4e50f6d5fb6a2caa01f1ce9dabd342
rabeeaatif <54802882+rabeeaatif@users.noreply.github.com>
2019-12-08 01:45:26 -0800

    Update Paper.hpp

--- Paper.hpp
+++ Paper.hpp
@@ -1,8 +1,14 @@
+#include <iostream>
 #include "Trash.hpp"
 
 class Paper : public Trash
 {
+private:
+public:
     Paper();
-    ~Paper();
-    void move();
+    Paper(LTexture *bodyImg, float x, float y);
+    virtual ~Paper();
+    virtual void Render(long int &frame, SDL_Renderer *gRenderer, bool debug);
+    virtual void Move();
+    void Draw(SDL_Renderer *gRenderer);
 };
